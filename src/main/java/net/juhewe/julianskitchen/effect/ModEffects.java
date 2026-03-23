545010e9cfcd215142e8df5f98b69c09b8dcc6a0
Julian Wenzel <julinator100@gmail.com>
2026-03-23 17:35:46 +0100

    Implemented Firewalk Effect

--- src/main/java/net/juhewe/julianskitchen/effect/ModEffects.java
+++ src/main/java/net/juhewe/julianskitchen/effect/ModEffects.java
@@ -41,6 +41,11 @@ public class ModEffects {
                     new SaturationEffect(StatusEffectCategory.BENEFICIAL, 0xF82423)
             );
 
+    public static final RegistryEntry<StatusEffect> FIREWALK =
+            registerStatusEffect(
+                    "firewalk",
+                    new FirewalkEffect(StatusEffectCategory.HARMFUL, 0xDEE600)
+            );
 
     private static RegistryEntry<StatusEffect> registerStatusEffect(String name, StatusEffect statusEffect){
         return Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(JuliansKitchen.MOD_ID, name), statusEffect);
